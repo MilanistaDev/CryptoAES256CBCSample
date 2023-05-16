9d83c45b47a886498bf045bdce65899375a85f37
Takuya Aso <milanista224@gmail.com>
2023-05-16 15:40:55 +0900

    Initial Commit

---
+++ CryptoAES256CBCSample/CryptoAES256CBCSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAES256CBCSampleApp.swift
+//  CryptoAES256CBCSample
+//
+//  Created by Takuya Aso on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAES256CBCSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
